e0fd105f51adb70dcaac7e47e491b0e8af480d72
ioskpu <lrcorales@gmail.com>
2023-11-21 15:33:37 -0500

    se agrega node-fetch para hacer la solicitud HTTP desde el servidor

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const Constants = require('../controllers/constants');
 const pjson = require('../package.json');
 const path = require('path');
+const fetch = require('node-fetch');
 
 // App definition
 const app = express();
@@ -20,8 +21,62 @@ app.get('/api', (req, res) => {
 });
 
 // Ruta para servir el archivo index.html
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'));
+app.get('/', async function(req, res) {
+    try {
+        const response = await fetch('https://quotes-api-pi.vercel.app/api/randomimage?language=es');
+        const data = await response.json();
+
+        const quoteImage = data.image;
+        const quoteText = data.text;
+
+        res.send(`
+            <html>
+            <head>
+                <title>Frases Aleatorias</title>
+                <style>
+                    /* Agrega tus estilos CSS aquí */
+                </style>
+            </head>
+            <body>
+                <h1>Frases Aleatorias</h1>
+                <div id="quote-container">
+                    <img id="quote-image" src="${quoteImage}" alt="Imagen Aleatoria">
+                    <p id="quote-text">${quoteText}</p>
+                </div>
+                <button id="new-quote-btn">Nueva Frase</button>
+        
+                <script>
+                    const newQuoteButton = document.getElementById('new-quote-btn');
+        
+                    // Función para obtener una nueva frase aleatoria
+                    function getNewQuote() {
+                        fetch('https://quotes-api-pi.vercel.app/api/randomimage?language=es')
+                            .then(response => response.json())
+                            .then(data => {
+                                const quoteImage = data.image;
+                                const quoteText = data.text;
+        
+                                document.getElementById('quote-image').src = quoteImage;
+                                document.getElementById('quote-text').textContent = quoteText;
+                            })
+                            .catch(error => {
+                                console.error('Error al obtener la frase:', error);
+                            });
+                    }
+        
+                    // Llama a la función para mostrar una frase al cargar la página
+                    getNewQuote();
+        
+                    // Agrega un event listener al botón para obtener una nueva frase al hacer clic
+                    newQuoteButton.addEventListener('click', getNewQuote);
+                </script>
+            </body>
+            </html>
+        `);
+    } catch (error) {
+        console.error('Error al obtener la frase:', error);
+        res.status(500).send('Error al obtener la frase');
+    }
 });
 
 console.log('Starting Server ...... ');
